f94862c5eb147c398a7f61271741f5c92d6d2f54
Tkachenko01001 <x380999660502@gmail.com>
2023-07-07 19:11:36 +0300

    fix imports

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from "./Redux/store";
+import { store } from "./Redux/Store/store";
 import { Provider } from "react-redux";
 
 import App from './App.jsx';
