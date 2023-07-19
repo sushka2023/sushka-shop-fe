421db480e1435ec19e5f7ad427d7a791ccfb8dbb
Tkachenko01001 <x380999660502@gmail.com>
2023-07-19 20:55:50 +0300

    fix/deploy_error

--- src/pages/main-page/MainPage.jsx
+++ src/pages/main-page/MainPage.jsx
@@ -1,14 +1,7 @@
-import { ReactSVG } from "react-svg";
-import svgSprite from "../../icons/symbol-defs.svg";
-
 const MainPage = () => {
   return (
-    <div className="test-container">
-      <ReactSVG src={svgSprite} />
-      <svg width={30} height={30}>
-        <use xlinkHref="#icon-inst" />
-      </svg>
-      <h1 className="test">Sushka shop TEST123</h1>
+    <div>
+      <h1>Sushka shop TEST123</h1>
     </div>
   );
 };
