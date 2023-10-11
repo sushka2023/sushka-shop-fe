f98f2e3eb344a80d83585f6f2453c8852166569e
Tkachenko01001 <x380999660502@gmail.com>
2023-10-12 01:07:37 +0300

    updt/base_src

--- src/index.jsx
+++ src/index.jsx
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/sushka-shop.netlify.app">
+        <BrowserRouter basename="/">
           <App />
         </BrowserRouter>
       </PersistGate>
