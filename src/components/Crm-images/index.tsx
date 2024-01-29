f5ec7c895d6653266900d480d8b328c0b6a50df7
Denis Tkachenko <113258288+Tkachenko01001@users.noreply.github.com>
2024-01-29 18:32:02 +0200

    Fix/add product after migration (#42)
    * fix validation
    
    * updt init price
    
    * fix

--- src/components/Crm-images/index.tsx
+++ src/components/Crm-images/index.tsx
@@ -8,7 +8,6 @@ import StarIcon from '../../icons/star.svg?react'
 import Notiflix from 'notiflix'
 import styles from './crmImages.module.scss'
 import {
-  ProductState,
   addData,
   incrementImagesUploadCount,
   resetImagesUploadCount,
@@ -17,6 +16,7 @@ import {
 import { newProductImagesSchema } from '../../helpers/validateNewProduct'
 import { addImages } from '../../redux/crm-add-new-product/operation'
 import { AppDispatch } from '../../redux/store'
+import { RootState } from '../../redux/store/index'
 
 const CrmImages = () => {
   const [activeFile, setActiveFile] = useState<string | null>(null)
@@ -26,8 +26,12 @@ const CrmImages = () => {
   const [attemptedUpload, setAttemptedUpload] = useState(false)
   const fileInputRef = useRef<HTMLInputElement>(null)
   const dispatch = useDispatch<AppDispatch>()
-  const productId = useSelector((state: ProductState) => state.productId)
-  const formErrors = useSelector((state: ProductState) => state.formErrors)
+  const productId = useSelector(
+    (state: RootState) => state.newProduct.productId
+  )
+  const formErrors = useSelector(
+    (state: RootState) => state.newProduct.formErrors
+  )
 
   useEffect(() => {
     if (filesArr.length > 4) {
