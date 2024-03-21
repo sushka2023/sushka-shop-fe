b9012afa6ab25f28065aa7f7d9bc7cc863134d23
Tkachenko01001 <x380999660502@gmail.com>
2024-03-21 21:14:18 +0200

    overflow_hidden_for_modal

--- src/components/modal-portal/ModalPortal.tsx
+++ src/components/modal-portal/ModalPortal.tsx
@@ -3,7 +3,7 @@ import { createPortal } from 'react-dom'
 import { CSSTransition, Transition } from 'react-transition-group'
 import IconClose from '../../icons/close.svg?react'
 import styles from './modal-portal.module.scss'
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../redux/store'
 import { resetAuth } from '../../redux/authentication/slice'
@@ -26,6 +26,12 @@ const ModalPortal: FC<Props> = ({
   const navigate = useNavigate()
   const [searchParams] = useSearchParams()
   const dispatch = useDispatch<AppDispatch>()
+
+  useEffect(() => {
+    isModalOpen
+      ? document.body.classList.add(styles.modalOpen)
+      : document.body.classList.remove(styles.modalOpen)
+  }, [isModalOpen])
 
   const closeModal = (e: React.MouseEvent<Element, MouseEvent>) => {
     if (e.target !== e.currentTarget) return
