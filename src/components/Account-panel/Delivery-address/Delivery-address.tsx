2ca1f6493ec9cdf3f5057cd1f6fa0eff30eec7b5
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-04-19 08:34:01 +0200

    add btn custom

--- src/components/Account-panel/Delivery-address/Delivery-address.tsx
+++ src/components/Account-panel/Delivery-address/Delivery-address.tsx
@@ -1,10 +1,10 @@
-import { Box, Grid, Paper, Typography, styled } from '@mui/material'
+import { Box, Button, Grid, Paper, Typography, styled } from '@mui/material'
 import { stH3, stP } from '../../auth/style'
 import AddIcon from '@mui/icons-material/Add'
 import IconNovaPoshta from '../../../icons/novaPoshta.svg?react'
 import IconUkrPoshta from '../../../icons/ukrPoshta.svg?react'
 // import IconPen from '../../../icons/pen.svg?react'
-import { DelivetyAddressBtn } from '../style'
+import CreateIcon from '@mui/icons-material/Create'
 export const DeliveryAddress = () => {
   const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -87,67 +87,35 @@ export const DeliveryAddress = () => {
               Київ, Київська область, Героїв Небесної сотні 25, кв.10
             </Typography>
           </Item>
-          <DelivetyAddressBtn> Редагувати</DelivetyAddressBtn>
-        </Grid>
-        <Grid item xs={12} md={6} lg={3}>
-          <Item
+          <Button
+            variant="outlined"
+            type="button"
+            endIcon={<CreateIcon />}
             sx={{
               width: 300,
-              height: 180,
+              height: 50,
+              borderRadius: 2.5,
+              color: '#FFFFFF',
+              backgroundColor: '#FCC812',
+              border: 'transparent',
+              cursor: 'pointer',
+              marginTop: 2.5,
+              fontSize: 14,
+              fontWeight: 700,
+              fontFamily: 'Open Sans',
               boxShadow: 'none',
-              borderRadius: 5,
-              padding: 0
+              [`&:hover`]: {
+                backgroundColor: '#FFFFFF',
+                color: '#FCC812',
+                border: '2px solid rgba(252, 200, 18, 0.8)',
+                boxShadow: 'none'
+              }
             }}
           >
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                color: '#567343',
-                padding: 2
-              }}
-            >
-              <IconNovaPoshta style={{ margin: '10px 20px 10px 10px' }} />
-              <Box>
-                <Typography
-                  variant="body1"
-                  sx={{
-                    fontSize: 22,
-                    fontWeight: 600,
-                    fontFamily: 'Open Sans'
-                  }}
-                >
-                  Нова пошта
-                </Typography>
-                <Typography
-                  variant="body2"
-                  sx={{
-                    fontSize: 16,
-                    fontFamily: 'Open Sans',
-                    textAlign: 'start',
-                    fontWeight: 400
-                  }}
-                >
-                  Адресна
-                </Typography>
-              </Box>
-            </Box>
-            <Typography
-              variant="body1"
-              sx={{
-                fontSize: 16,
-                fontWeight: 400,
-                fontFamily: 'Open Sans',
-                color: '#567343',
-                textAlign: 'start',
-                margin: '0 15px'
-              }}
-            >
-              Київ, Київська область, Героїв Небесної сотні 25, кв.10
-            </Typography>
-          </Item>
-          <DelivetyAddressBtn> Редагувати</DelivetyAddressBtn>
+            Редагувати
+          </Button>
         </Grid>
+
         <Grid item xs={12} md={6} lg={3}>
           <Item
             sx={{
@@ -205,9 +173,34 @@ export const DeliveryAddress = () => {
               Київ, Київська область, Героїв Небесної сотні 25, кв.10
             </Typography>
           </Item>
-          <DelivetyAddressBtn> Редагувати</DelivetyAddressBtn>
+          <Button
+            variant="outlined"
+            type="button"
+            endIcon={<CreateIcon />}
+            sx={{
+              width: 300,
+              height: 50,
+              borderRadius: 2.5,
+              color: '#FFFFFF',
+              backgroundColor: '#FCC812',
+              border: 'transparent',
+              cursor: 'pointer',
+              marginTop: 2.5,
+              fontSize: 14,
+              fontWeight: 700,
+              fontFamily: 'Open Sans',
+              boxShadow: 'none',
+              [`&:hover`]: {
+                backgroundColor: '#FFFFFF',
+                color: '#FCC812',
+                border: '2px solid rgba(252, 200, 18, 0.8)',
+                boxShadow: 'none'
+              }
+            }}
+          >
+            Редагувати
+          </Button>
         </Grid>
-
         <Grid item xs={12} md={6} lg={3}>
           <Item
             sx={{
--- src/components/Account-panel/style.ts
+++ src/components/Account-panel/style.ts
@@ -20,24 +20,3 @@ export const ContactInfoBtn = styled(Button)({
     boxShadow: 'none'
   }
 })
-export const DelivetyAddressBtn = styled(Button)({
-  'width': 300,
-  'height': 50,
-  'borderRadius': 10,
-  'color': '#FFFFFF',
-  'backgroundColor': '#FCC812',
-  'border': 'none',
-  'cursor': 'pointer',
-  'marginTop': 20,
-  'fontSize': 14,
-  'fontWeight': 700,
-  'fontFamily': 'Open Sans',
-  'boxShadow': 'none',
-  'transition': 'background-color 0.3s ease-in-out',
-  '&:hover': {
-    backgroundColor: '#FFFFFF',
-    color: '#FCC812',
-    border: '2px solid rgba(252, 200, 18, 0.8)',
-    boxShadow: 'none'
-  }
-})
