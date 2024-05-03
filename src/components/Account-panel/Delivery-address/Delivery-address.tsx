8f3325dbffd754c5d2558860c987d89d5d8a0315
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-05-03 14:37:54 +0200

    corect logics for parcel loker and office

--- src/components/Account-panel/Delivery-address/Delivery-address.tsx
+++ src/components/Account-panel/Delivery-address/Delivery-address.tsx
@@ -55,39 +55,44 @@ const getAddressTextAndIcon = (event: AddressItem): AddressInfo => {
     source,
     address_warehouse: addressWarehouse = undefined
   } = event
-
-  const text = type
   let icon
   let addressHeadline
   let location
   let titleAddress
 
-  switch (source) {
-    case 'nova_poshta':
-      icon = <IconNovaPoshta style={stIconM} />
-      addressHeadline = 'Нова пошта'
-      break
-    case 'ukr_poshta':
-      icon = <IconUkrPoshta style={stIconM} />
-      addressHeadline = 'Укрпошта'
-      location = 'Відділення'
-      titleAddress = `${event.city}, ${event.street}, ${event.region}, ${event.post_code}, буд.${event.house_number}, кв.${event.apartment_number}`
-      break
-    default:
-      break
+  const setIconAndHeadline = (source: string) => {
+    switch (source) {
+      case 'nova_poshta':
+        icon = <IconNovaPoshta style={stIconM} />
+        addressHeadline = 'Нова пошта'
+        break
+      case 'ukr_poshta':
+        icon = <IconUkrPoshta style={stIconM} />
+        addressHeadline = 'Укрпошта'
+        location = 'Відділення'
+        titleAddress = `${event.city}, ${event.street}, ${event.region}, ${event.post_code}, буд.${event.house_number}, кв.${event.apartment_number}`
+        break
+      default:
+        break
+    }
   }
 
-  if (addressType === 'адреса') {
-    location = 'Адреса'
-    titleAddress = `${event.city}, ${event.street}, буд.${event.house_number}, кв.${event.apartment_number}`
-  } else if (addressType === 'відділення') {
-    const location: string = determineLocation(addressWarehouse!)
-    titleAddress = `${location} ${event.address_warehouse}, ${event.city}`
+  const setLocationAndAddress = () => {
+    if (addressType === 'адреса') {
+      location = 'Адреса'
+      titleAddress = `${event.city}, ${event.street}, буд.${event.house_number}, кв.${event.apartment_number}`
+    } else if (addressType === 'відділення') {
+      location = determineLocation(addressWarehouse ?? undefined)
+      titleAddress = `${location} ${event.address_warehouse}, ${event.city}`
+    }
   }
+
+  setIconAndHeadline(source)
+  setLocationAndAddress()
 
   return {
     icon,
-    text,
+    text: type,
     addressHeadline,
     location,
     titleAddress,
