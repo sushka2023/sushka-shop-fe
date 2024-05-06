e3782be8d52735fddbc6cfce632b884caffb96a4
Tkachenko01001 <x380999660502@gmail.com>
2024-05-07 02:01:19 +0300

    remove/radio

--- src/lib/mui/config/overrides/radio.ts
+++
@@ -1,7 +0,0 @@
-import { Components, Theme } from '@mui/material'
-
-const MuiRadio: Components<Theme>['MuiRadio'] = {
-  styleOverrides: {}
-}
-
-export { MuiRadio }
--- src/lib/mui/config/theme/components.ts
+++ src/lib/mui/config/theme/components.ts
@@ -3,15 +3,13 @@ import { MuiButton } from '../overrides/button'
 import { MuiOutlinedInput } from '../overrides/input'
 import { MuiInputLabel } from '../overrides/label'
 import { MuiAutocomplete } from '../overrides/autocomplete'
-import { MuiRadio } from '../overrides/radio'
 
 const COMPONENTS: Pick<ThemeOptions, 'components'> = {
   components: {
     MuiButton,
     MuiOutlinedInput,
     MuiInputLabel,
-    MuiAutocomplete,
-    MuiRadio
+    MuiAutocomplete
   }
 }
 
