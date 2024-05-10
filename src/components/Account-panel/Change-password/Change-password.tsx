9c779f49b8ba0f7791529cdbee4d20712c403e0f
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-05-10 12:43:03 +0200

    create logo and input

--- src/components/Account-panel/Change-password/Change-password.tsx
+++ src/components/Account-panel/Change-password/Change-password.tsx
@@ -1,3 +1,17 @@
+import { Box, Typography } from '@mui/material'
+import { stH3, stP } from '../../auth/style'
+
 export const ChangePassword = () => {
-  return <div>ChangePassword</div>
+  return (
+    <Box>
+      <Box>
+        <Typography variant="h3" sx={stH3}>
+          Ваша контактна інформація
+        </Typography>
+        <Typography variant="body1" sx={stP}>
+          Тут ви можете змінити ваші дані
+        </Typography>
+      </Box>
+    </Box>
+  )
 }
