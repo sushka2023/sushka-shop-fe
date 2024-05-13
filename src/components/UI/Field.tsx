187db4fd94185ae28d88f959c5eb2b4abee728eb
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-05-13 19:00:53 +0200

    deBug field

--- src/components/UI/Field.tsx
+++ src/components/UI/Field.tsx
@@ -1,11 +1,13 @@
+import React from 'react'
 import {
   OutlinedInput as OutlinedInputMui,
   OutlinedInputProps
 } from '@mui/material'
-import { FC } from 'react'
 
-const OutlinedInput: FC<OutlinedInputProps> = ({ ...props }) => {
-  return <OutlinedInputMui {...props} />
-}
+const OutlinedInput = React.forwardRef<HTMLInputElement, OutlinedInputProps>(
+  (props, ref) => {
+    return <OutlinedInputMui {...props} ref={ref} />
+  }
+)
 
 export { OutlinedInput }
