71af5b137b7b33f15b88dd5dbc1d1b570530681b
San88Francisco <136508377+San88Francisco@users.noreply.github.com>
2024-05-13 21:41:22 +0200

    create btn communication

--- src/components/Account-panel/Order-history/Order-history.tsx
+++ src/components/Account-panel/Order-history/Order-history.tsx
@@ -2,20 +2,31 @@ import { Box, Typography } from '@mui/material'
 import { stH3 } from '../../auth/style'
 import React, { useEffect } from 'react'
 import axiosInstance from '../../../axios/settings'
-
-const items = [0, 1, 2, 3, 4]
+import { Button } from '../../UI/Button'
+import CreateIcon from '@mui/icons-material/Create'
+const items = [0, 1, 2, 3]
 const divToInsert = (
   <Box
     key="inserted_div"
-    sx={{ border: '1px solid #FEEEE1', width: '80%', margin: '0 auto' }}
+    sx={{ border: '1px solid #FEEEE1', width: '80%', margin: '10 auto' }}
   ></Box>
 )
 const elementsWithDiv = items.map((item, index) => (
   <React.Fragment key={index}>
     {index > 0 && divToInsert}
-    <Box sx={{ height: 40 }}>{item} hello</Box>
+    <Box
+      sx={{
+        height: 150,
+        display: 'flex',
+        alignItems: 'center',
+        cursor: 'pointer'
+      }}
+    >
+      {item} hello
+    </Box>
   </React.Fragment>
 ))
+
 export const OrderHistory = () => {
   useEffect(() => {
     const fetchOrders = async () => {
@@ -38,7 +49,7 @@ export const OrderHistory = () => {
         )
         console.log('✌️response --->', response)
       } catch (error) {
-        console.error('❌ Error fetching orders:', error)
+        console.error('Error fetching orders:', error)
       }
     }
 
@@ -50,14 +61,31 @@ export const OrderHistory = () => {
       <Box
         sx={{
           display: 'flex',
-          border: '1px solid',
           justifyContent: 'space-between'
         }}
       >
-        <Typography variant="h3" sx={stH3}>
+        <Typography variant="h3" sx={{ ...stH3 }}>
           Ваші замовлення
         </Typography>
-        <button>Зв’яжіться з нами</button>
+        <Button
+          sx={{
+            'padding': '10px 20px',
+            'borderRadius': 6,
+            'backgroundColor': '#FFFFFF',
+            'color': '#64748B',
+            'border': '#567343',
+            'fontSize': 14,
+            'fontWeight': 700,
+            '&:hover': {
+              backgroundColor: '#64748B',
+              color: '#FFFFFF',
+              border: '#FFFFFF'
+            }
+          }}
+        >
+          Зв’яжіться з нами
+          <CreateIcon sx={{ fontSize: 20, ml: 1 }} />
+        </Button>
       </Box>
       <Box
         sx={{
