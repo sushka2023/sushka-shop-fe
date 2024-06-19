a2850473349b7cb008648f840fafa0d15677a5db
YakovchukIvan <81960402+YakovchukIvan@users.noreply.github.com>
2024-06-19 21:03:57 +0300

    Edit container theme MUI

--- src/lib/mui/config/overrides/container.ts
+++ src/lib/mui/config/overrides/container.ts
@@ -2,7 +2,7 @@ import { Components, Theme } from '@mui/material'
 const MuiContainer: Components<Theme>['MuiContainer'] = {
   styleOverrides: {
     root: {
-      padding: '0 clamp(0.875rem, 0.429rem + 1.19vw, 1.5rem)'
+      padding: '0 clamp(0.875rem, 0.429rem + 1.19vw, 1.5rem) !important'
     }
   }
 }
