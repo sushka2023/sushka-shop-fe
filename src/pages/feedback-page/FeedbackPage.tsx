c5a0802212c2e64ef1d5eb47825d23d9ec55dc60
sivlvy <vladislavsidenko@ukr.net>
2024-07-25 21:48:27 +0300

    adaptive for mob/tablet/desk

--- src/pages/feedback-page/FeedbackPage.tsx
+++ src/pages/feedback-page/FeedbackPage.tsx
@@ -1,13 +1,20 @@
 import FeedbackForm from '../../components/feedbacks/form/FeedbackForm'
 import styles from './FeedbackPage.module.scss'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchReviews } from '../../redux/feedbacks/operations'
 import { useDispatch } from 'react-redux'
 import FeedbackItem from '../../components/feedbacks/list/FeedbackItem'
+import { ModalCustom } from '../../components/Modal-custom-btn/ModalCustomWindow'
 
 const LIMIT = 5
 const OFFSET = 0
 const FeedbackPage = () => {
+  const [openModal, setOpenModal] = useState<boolean>(false)
+
+  const handleOpenModal = () => {
+    setOpenModal(true)
+  }
+
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(fetchReviews({ limit: LIMIT, offset: OFFSET }))
@@ -16,12 +23,24 @@ const FeedbackPage = () => {
 
   return (
     <div className={styles.container}>
-      <h2 className={styles.title}>Відгуки</h2>
-      <div className={styles.feedbacksWrapper}>
-        <ul className={styles.feedbackList}>
-          <FeedbackItem reviews={[]} />
-        </ul>
-        <FeedbackForm />
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Відгуки</h2>
+        <button
+          type="button"
+          className={styles.button}
+          onClick={handleOpenModal}
+        >
+          Залишити відгук
+        </button>
+        <div className={styles.feedbacksWrapper}>
+          <ul className={styles.feedbackList}>
+            <FeedbackItem reviews={[]} />
+          </ul>
+          <ModalCustom openModal={openModal} setOpenModal={setOpenModal}>
+            <FeedbackForm />
+          </ModalCustom>
+          <FeedbackForm />
+        </div>
       </div>
     </div>
   )
