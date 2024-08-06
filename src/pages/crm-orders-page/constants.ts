2d07a46b5a4a2a0c5f13f765b929117b3644e4b4
Tkachenko01001 <x380999660502@gmail.com>
2024-08-06 19:58:41 +0300

    fix/order_status_type

--- src/pages/crm-orders-page/constants.ts
+++ src/pages/crm-orders-page/constants.ts
@@ -9,23 +9,23 @@ type OrderStatusValue = {
 }
 
 export const ORDER_STATUS: { [key in OrdersStatus]: OrderStatusValue } = {
-  'new': {
+  [OrdersStatus.NEW]: {
     text: 'Новий',
     style: { backgroundColor: '#EFF3FF', color: '#5D5FEF' }
   },
-  'in processing': {
+  [OrdersStatus.IN_PROCESSING]: {
     text: 'В обробці',
     style: { backgroundColor: '#FFF9E3', color: '#E07706' }
   },
-  'shipped': {
+  [OrdersStatus.SHIPPED]: {
     text: 'Відправлено',
     style: { backgroundColor: '#EFF9FF', color: '#178DCC' }
   },
-  'delivered': {
+  [OrdersStatus.DELIVERED]: {
     text: 'Доставлено',
     style: { backgroundColor: '#E8FDF2', color: '#059691' }
   },
-  'cancelled': {
+  [OrdersStatus.CANCELLED]: {
     text: 'Скасовано',
     style: { backgroundColor: '#F6E5EF', color: '#E11D48' }
   }
